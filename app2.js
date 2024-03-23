c30372b2c4fdd2dc5ac1ada87aab1252164af50c
Mayank3107 <mayank.gupta_cs22@gla.ac.in>
2024-03-23 21:50:35 +0530

    about section added

--- app2.js
+++ app2.js
@@ -1,3 +1,20 @@
+var modal=document.getElementById("aboutModal");
+var btn=document.getElementById("aboutButton");
+var span=document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick=function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event){
+if(event.target==modal){
+    modal.style.display = "none";
+    }
+}
+
 let registers = {
     A: 0,
     B: 0,
